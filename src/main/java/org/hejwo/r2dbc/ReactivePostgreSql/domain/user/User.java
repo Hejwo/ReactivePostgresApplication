c55690b69e279bb5ec810ead8b48cca3947bcf57
Piotr Hejwowski <piotr.hejwowski@softwaremill.pl>
2019-01-26 13:45:43 +0100

    User aggregate - added User and ReactiveRepository for it.

---
+++ src/main/java/org/hejwo/r2dbc/ReactivePostgreSql/domain/user/User.java
@@ -0,0 +1,23 @@
+package org.hejwo.r2dbc.ReactivePostgreSql.domain.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hejwo.r2dbc.ReactivePostgreSql.domain.common.Aggregate;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class User implements Aggregate {
+
+    @Id
+    private Long id;
+    private String uuid;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String mail;
+    private LocalDateTime created;
+
+}
---
+++ src/main/java/org/hejwo/r2dbc/ReactivePostgreSql/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package org.hejwo.r2dbc.ReactivePostgreSql.domain.user;
+
+import org.hejwo.r2dbc.ReactivePostgreSql.domain.common.AggregateRepository;
+
+interface UserRepository extends AggregateRepository<User> {
+}
